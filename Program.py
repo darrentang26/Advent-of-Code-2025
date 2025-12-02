2c2e62601ce4bb90889ea6239e43c0d463c256a2
Darren Tang <darrentang@Darrens-MacBook-Air.local>
2025-12-02 14:54:34 -0600

    Initial commit

---
+++ Program.py
@@ -0,0 +1,9 @@
+def main():
+    dial_pos = 50
+    with open("../input.txt", "r"):
+        for line in input:
+            direction = "+" if "R" else "-"
+            
+
+if __name__ == "__main__":
+    main()
