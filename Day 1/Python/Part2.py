8e5cf2d7a8128cc391ef30dd9d8b017b4f1d4684
Darren Tang <darrenvtang@gmail.com>
2025-12-04 21:02:04 -0600

    Adding day 2

--- Day 1/Python/Part2.py
+++ Day 1/Python/Part2.py
@@ -4,27 +4,41 @@ def main():
     with open("../input.txt", "r") as input:
         for line in input:
             move = 0
+            zero_start = dial_pos == 0
             if len(line) == 2:
                 move = int(line[1])
             elif len(line) == 3:
                 move = int(line[1:3])
-            else:
+            elif len(line) == 4:
                 move = int(line[1:4])
+            else:
+                move = int(line[1:5])
+            if dial_pos == 0 and move % 100 == 0:
+                print(str(dial_pos) + "," + str(move))
+            print("Dial Position: " + str(dial_pos))
+            print("Line: " + line)
+            print("Move: " + str(move))
+            print("Old Zero Count: " + str(zero_count))
+            zero_count += move // 100
+            print("New Zero Count: " + str(zero_count))
             move %= 100 
-            print(line[:-1] + " | " + str(move))
+            print("New Move: " + str(move))
             if line[0] == "R":
                 dial_pos += move
             else:
                 dial_pos -= move
+            print("Pre-shift dial position " + str(dial_pos))
             if dial_pos > 99:
                 dial_pos = dial_pos - 100
+                zero_count += 1
             elif dial_pos < 0:
                 dial_pos = dial_pos + 100
-            if dial_pos == 0:
+                if not zero_start:
+                    zero_count += 1
+            elif dial_pos == 0:
                 zero_count += 1
-            if dial_pos > 99 or dial_pos < 0:
-                print("Dial out of bounds")
-            print("Dial Position: " + str(dial_pos))
+            print("New Dial Position: " + str(dial_pos))
+            print("-----------------------------------------")
         print("Zeros counted:  " + str(zero_count))
 
 if __name__ == "__main__":
---
+++ Day 2/C#/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+public class Day2
+{
+    public static void main(string[] args)
+    {
+        using (StreamReader sr = new StreamReader("../Input.txt"))
+        {
+            string Input = fs.ReadLine();
+            string[] ranges = Input.Split(',');
+            foreach(string range in ranges)
+            {
+                
+            }
+        }
+    }
+
+    public int[] FindInvalidIDs(string range)
+    {
+        string bounds = range.Split("-");
+        int lower = int.parse(bounds[0]);
+        int upper = int.parse(bounds[1]);
+    }
+}
