129cef1571558c56562f2eebb1cb1a1c8d1c379e
Darren Tang <darrentang@Darrens-MacBook-Air.local>
2025-12-05 17:45:19 -0600

    Worked on Day 2

--- Day 2/C#/Program.cs
+++ Day 2/C#/Program.cs
@@ -1,25 +1,49 @@
 ﻿using System;
 using System.IO;
+using System.Collections;
 
 public class Day2
 {
     public static void main(string[] args)
     {
+        int sum = 0;
+        List<int> invalids = new List<int>();
+        
         using (StreamReader sr = new StreamReader("../Input.txt"))
         {
             string Input = fs.ReadLine();
             string[] ranges = Input.Split(',');
             foreach(string range in ranges)
             {
-                
+                invalids.AddRange(FindInvalidIDsInRange(range));
+                foreach (int i in invalids)
+                {
+                    sum += i;
+                }
             }
         }
+        
+        Console.WriteLine("Invalid Sums: " + sum.ToString());
     }
 
-    public int[] FindInvalidIDs(string range)
+    internal List<int> FindInvalidIDsInRange(string range)
     {
         string bounds = range.Split("-");
         int lower = int.parse(bounds[0]);
         int upper = int.parse(bounds[1]);
+        List<int> invalids = new List<int>();
+        for (int i = lower; i <= upper; i++)
+        {
+            if (FullyRepeats(i.ToString()))
+            {
+                invalids.add(i);
+            }
+        }
+        return invalids;
+    }
+
+    internal bool FullyRepeats(string ID)
+    {
+        
     }
 }
